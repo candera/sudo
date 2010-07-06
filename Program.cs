37adbdee8987364fbb70455f31dc5221d6cdba61
Craig Andera <candera@wangdera.com>
2010-07-06 06:25:52 -0400

    Initial add

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Wangdera.SuDo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var psi = new ProcessStartInfo(args[0]);
+            psi.Verb = "runas";
+
+            if (args.Length > 1)
+            {
+                psi.Arguments = string.Join(" ", args.Skip(1).ToArray());
+            }
+
+            Process.Start(psi); 
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SuDo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Wangdera Corporation")]
+[assembly: AssemblyProduct("MySuDo")]
+[assembly: AssemblyCopyright("Copyright © Wangdera Corporation 2010 (sudo@wangdera.com)")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("787510e6-374e-44ab-98f6-a2b844d8ea98")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
